bc2ec64b00559621c61e8977f72ba1d2f3ef57ae
Aquil <aquil@Aquils-MacBook-Pro.local>
2022-02-04 15:21:01 +0530

    first commit

---
+++ app/Http/Controllers/usercontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class usercontroller extends Controller
+{
+    //
+    function show($id)
+    {
+        return $id;
+    }
+}
